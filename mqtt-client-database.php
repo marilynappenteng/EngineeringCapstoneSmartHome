bd13920ac4498a4a5ee11b10c4ce2b345effe10f
MHAcPkPEN <marappen1@gmail.com>
2023-04-01 16:27:42 +0000

    Updates to php
    ultrasonic was not uploading to database so I changed the variable name in php and it worked

--- mqtt-client-database.php
+++ mqtt-client-database.php
@@ -9,15 +9,15 @@ $con = mysqli_connect($servername, $username, $password,$dbname);
 
 $ldr1=$_GET['ldr1'];
 $ldr2=$_GET['ldr2'];
-$ultrasonic1=$_GET['ultrasonic1'];
-$ultrasonic2=$_GET['ultrasonic2'];
+$door1ultra1=$_GET['ultrasonic1'];
+$door1ultra2=$_GET['ultrasonic2'];
 $housevoltage=$_GET['housevoltage'];
 $housecurrent=$_GET['housecurrent'];
 $kitchenflow = $_GET['kitchenflow'];
 $kitchendistance = $_GET['kitchendistance'];
 
 $sql = "INSERT INTO `ldr` (`ldr_reading1`, `ldr_reading2`) VALUES ('{$ldr1}', '{$ldr2}')";
-$sql2 = "INSERT INTO `door`(`ultrasonic11`, `ultrasonic2`) VALUES ('{$ultrasonic1}','{$ultrasonic2}')";
+$sql2 = "INSERT INTO `door`(`ultrasonic1`, `ultrasonic2`) VALUES ('{$door1ultra1}','{$door1ultra2}')";
 $sql3 = "INSERT INTO `voltage_and_current`(`voltage`, `current`) VALUES ('{$housevoltage}', '{$housecurrent}')";
 $sql4 = "INSERT INTO `water_flow_ultrasonic`(`flow_rate`,`distance`) VALUES ('{$kitchenflow}', '{$kitchendistance}')";
 
